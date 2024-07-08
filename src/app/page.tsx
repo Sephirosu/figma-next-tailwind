79e26195be9d47db8d16d268cc138517be8a2a3b
Sephirosu <irfansahman88@gmail.com>
2024-07-08 14:58:31 +0200

    installed icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { FaBeer } from "react-icons/fa";
+
 const App = () => {
-  return <div className="font-poppins ml-2 text-lg">Gagi</div>;
+  return (
+    <div className="font-poppins ml-2 text-lg">
+      Gagi <FaBeer></FaBeer>
+    </div>
+  );
 };
 
 export default App;
