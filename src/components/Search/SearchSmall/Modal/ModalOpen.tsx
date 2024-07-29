c21b6362d77d17fd0f3179e97d778c4cdae57cec
Sephirosu <irfansahman88@gmail.com>
2024-07-29 09:36:34 +0200

    scroll bug

--- src/components/Search/SearchSmall/Modal/ModalOpen.tsx
+++ src/components/Search/SearchSmall/Modal/ModalOpen.tsx
@@ -8,21 +8,22 @@ const ModalOpen: React.FC<{
   stopPropagation: (e: React.MouseEvent<HTMLDivElement>) => void;
 }> = ({ closeModal, stopPropagation }) => {
   useEffect(() => {
-    document.body.classList.add("overflow-hidden");
+    const originalStyle = window.getComputedStyle(document.body).overflow;
+    document.body.style.overflow = "hidden";
 
     return () => {
-      document.body.classList.remove("overflow-hidden");
+      document.body.style.overflow = originalStyle;
     };
   }, []);
 
   return (
     <div
       onClick={stopPropagation}
-      className="fixed inset-0 overflow-hidden  bg-black bg-opacity-50 flex justify-center items-center font-poppins"
+      className="fixed inset-0 overflow-hidden bg-black bg-opacity-50 flex justify-center items-center font-poppins"
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className="bg-gray-100 h-full w-full  shadow-lg overflow-hidden"
+        className="bg-gray-100 h-full w-full shadow-lg overflow-auto"
       >
         <CloseModalButton closeModal={closeModal} />
         <ModalContent />
