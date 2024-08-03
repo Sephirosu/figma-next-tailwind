a2ba4c0dc98bc60089a074d8cdc03dd9134cf216
Sephirosu <irfansahman88@gmail.com>
2024-08-03 12:24:34 +0200

    added color of heart when liked

--- src/components/Property/PictureHeart.tsx
+++ src/components/Property/PictureHeart.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
 import Heart from "@icons/heart.svg";
 
-
 const PictureHeart: React.FC = () => {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleClick = () => {
+    setIsLiked((prev) => !prev);
+  };
+
   return (
     <div>
-      <Heart className="absolute top-[15px] right-[15px] cursor-pointer hover:scale-105" />
+      <Heart
+        onClick={handleClick}
+        className="absolute top-[15px] right-[15px] cursor-pointer hover:scale-105"
+        style={{ fill: isLiked ? "red" : "gray" }}
+      />
     </div>
   );
 };
